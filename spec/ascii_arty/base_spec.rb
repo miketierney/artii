f16d6bba0aed5ab9aab43a34f54475e3103a2d23
Mike Tierney <mike@panpainter.com>
2010-08-26 10:01:51 -0700

    Making the AA::Base specs less redundant

--- spec/ascii_arty/base_spec.rb
+++ spec/ascii_arty/base_spec.rb
@@ -1,23 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe AsciiArty::Base do
+  before(:each) do
+    @arty = AsciiArty::Base.new
+  end
   describe '#new' do
     it "should create a new instance" do
-      arty = AsciiArty::Base.new
-      arty.should be_an_instance_of(AsciiArty::Base)
+      @arty.should be_an_instance_of(AsciiArty::Base)
     end
 
     it "should have an empty output" do
-      arty = AsciiArty::Base.new
-      arty.output.should be_empty
+      @arty.output.should be_empty
     end
   end
 
   describe "#asciify" do
     it "should produce an output" do
-      arty = AsciiArty::Base.new
-      arty.asciify 'hello world'
-      arty.output.should_not be_empty
+      @arty.asciify 'hello world'
+      @arty.output.should_not be_empty
     end
   end
 end
